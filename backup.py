e662c7b2c9e3e4dff44db26775b49c037024c960
DanSjukdom <daniel.py.216@gmail.com>
2020-09-05 00:59:38 -0500

    Dropbox connection working, upload zip argument

--- backup.py
+++ backup.py
@@ -1,7 +1,10 @@
 from settings import IGNORE_DIRS, IGNORE_FILES
+from DBoxConnection import DBoxConnection
 from datetime import datetime, time
+from crontab import CronTab
 from pathlib import Path
 import logging
+import dropbox
 import shutil
 import click
 import os
@@ -39,18 +42,18 @@ import os
     type=click.BOOL,
     required=True
 )
-def main(from_dir, to_dir, backup_start, frequency_interval, automatic_backup):
+@click.option(
+    "--upload-zip",
+    help="If the backup will be uploaded as a zipped file",
+    type=click.BOOL,
+    required=True
+)
+def main(from_dir, to_dir, backup_start, frequency_interval, automatic_backup, upload_zip):
     """
         A command line backup tool to keep your files stored at dropbox by using its API. 
         It runs at given time intervals specified by the user and can perform automatic
         backups using cronjobs
     """
-
-    logging.basicConfig(
-        filename="./backup.log",
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        level=logging.INFO
-    )
 
     FROM_DIR = Path(from_dir)
     TO_DIR = Path(to_dir)
@@ -58,36 +61,84 @@ def main(from_dir, to_dir, backup_start, frequency_interval, automatic_backup):
     FREQUENCY_INTERVAL = frequency_interval
     AUTOMATIC_BACKUP = automatic_backup
 
+    logging.basicConfig(
+        filename="./backup.log",
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=logging.INFO
+    )
+
+
+    # Dropbox connection object
+    # Handling the backup folder creation skip if already exist 
+    dbox = DBoxConnection()
+    
+    if dbox.validate_token():
+        logging.info("The dropbox auth token is valid")
+    else:
+        logging.error("The dropbox token is not valid, get a new one")
+
+    if TO_DIR.name not in dbox.get_dirs("", only_names=True, recursive=False):
+        dbox.create_folder(TO_DIR)
+        logging.info("The backup directory %s was created in dropbox")
+    else:
+        logging.warning("The backup directory %s already exists in dropbox", TO_DIR)
+
+
+    # Setting up the cron jobs 
+    # cron = CronTab(user=True)
+    # job = cron.new(command="")
+    # job.setall(datetime(year, month, day, hour, minute))
+    # datetime.strptime(date_string=)
+
     click.secho(f"Backing up {str(FROM_DIR)} into {str(TO_DIR)} at {str(BACKUP_START_TIME)} every {str(FREQUENCY_INTERVAL)}", fg="green")
 
+    # Traverse the directory to backup, skip the files and dirs that must be ignored
     VISITED = []
     TMP_DIR = Path.cwd()/"tmp"
     try:
         os.mkdir(TMP_DIR)
     except:
         pass
-    BUILD_PATH = list(TMP_DIR.parts)
+
+    if upload_zip:
+        BUILD_PATH = list(TMP_DIR.parts)
+    else:
+        BUILD_PATH = list(TO_DIR.parts)
+
     print("Starting build path = ", BUILD_PATH)
 
-    # Start from the root directory
     for root, dirs, files in os.walk(FROM_DIR):
         CURRENT_DIR = os.path.basename(root)
         logging.info("Visiting %s", root)
+        print("Build path -> ", BUILD_PATH)
         if CURRENT_DIR in VISITED:
             VISITED.remove(CURRENT_DIR)
-            PARENT_DIR = "/".join(BUILD_PATH)+"/"
-            if CURRENT_DIR not in os.listdir(PARENT_DIR):
-                _ = BUILD_PATH.pop()
+            PARENT_DIR = ("/".join(BUILD_PATH))[1:]
+            print(PARENT_DIR)
+            if upload_zip:
+                if CURRENT_DIR not in os.listdir(PARENT_DIR):
+                    _ = BUILD_PATH.pop()
+            else:
+                if CURRENT_DIR not in dbox.get_dirs(PARENT_DIR, only_names=True, recursive=False):
+                    _ = BUILD_PATH.pop()
             BUILD_PATH.append(CURRENT_DIR)
-            # logging.info("Visiting %s", "/".join(BUILD_PATH)[1:])
-
         
         if files:
             os.chdir(root)
             logging.info("%s has %s files", CURRENT_DIR, len(files))
             for FILE in files:
+                print("File = ", FILE)
                 if FILE not in IGNORE_FILES:
-                    shutil.copy(FILE, "/".join(BUILD_PATH)[1:]+"/")   # This the same [1]
+                    if upload_zip:
+                        shutil.copy(FILE, "/".join(BUILD_PATH)[1:]+"/")
+                    else:            
+                        with open(FILE, 'rb') as f:
+                            try:
+                                dbox.upload_content(file=f.read(), path="/".join(BUILD_PATH)[1:]+"/"+f.name)
+                                print("File uploaded")
+                            except dropbox.exceptions.ApiError as error:
+                                if error.error.is_path():
+                                    logging.error("Path error")
                 else:
                     logging.warning("Ignoring the file %s", FILE)
 
@@ -97,9 +148,13 @@ def main(from_dir, to_dir, backup_start, frequency_interval, automatic_backup):
                 if DIR not in IGNORE_DIRS:
                     VISITED.append(DIR)
                     try:
-                        os.mkdir("/".join(BUILD_PATH)[1:]+"/"+DIR)    # This the same [1]
+                        if upload_zip:
+                            os.mkdir("/".join(BUILD_PATH)[1:]+"/"+DIR)
+                        else:
+                            dbox.create_folder("/".join(BUILD_PATH)[1:]+"/"+DIR)    
+                        logging.info("Creating folder %s at %s", DIR, "/".join(BUILD_PATH)[1:])
                     except:
-                        pass
+                        logging.warning("Folder %s already exist at %s", DIR, "/".join(BUILD_PATH)[1:])
                 else:
                     logging.warning("Ignoring %s directory", DIR)
         else:
@@ -110,3 +165,6 @@ def main(from_dir, to_dir, backup_start, frequency_interval, automatic_backup):
 
 if __name__ == "__main__":
     main()
+
+
+# python backup.py --from-dir "/home/sjukdom/_Narasimha_/_forbackup_/" --to-dir "/Backup" --backup-start 60 --frequency-interval 120 --automatic-backup false
